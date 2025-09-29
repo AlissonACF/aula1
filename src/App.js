6fdc195be4f6fd8683d9a8b5d3415f1c9308aecb
Alisson Fagundes <alissomacf@gmail.com>
2025-09-29 19:45:50 -0300

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  function App() {
+   const [nome, setNome] = useState("");
+   const [email, setEmail] = useState("");
+   const [telefone, setTelefone] = useState("");
+  
+   const handleSubmit = (e) => {
+     e.preventDefault();
+      console.log({nome});
+      console.log({email});
+      console.log({telefone});
+   }
+
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>Nome:</label>
+          <input id='nome' type="text" value={nome} onChange={(e) => setNome(e.target.value)} />
+          <br />
+          <label>Email:</label>
+          <input id='email' type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <br />
+          <label>Telefone:</label>
+          <input id='telefone' type="tel" value={telefone} onChange={(e) => setTelefone(e.target.value)} />
+          <br />
+          <button type="submit">Enviar</button>
+          <br />
+          <label>{nome}</label>
+          <br />
+          <label>{email}</label>
+          <br />
+          <label>{telefone}</label>
+        </form>
+      </div>
+    )
+  }
 
 export default App;
